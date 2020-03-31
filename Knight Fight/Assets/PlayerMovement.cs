caa6441e4afaf8d83f06ff0b093136efb985deb2
Xiigor <47105441+Xiigor@users.noreply.github.com>
2020-03-31 12:56:43 +0200

    basic player movement

---
+++ Knight Fight/Assets/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour 
+{
+    public float movementSpeedMultiplier = 5.0f;
+    PlayerControls controls;
+    Vector2 move;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += ctx => move = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        Vector3 m = new Vector3(move.x, 0.0f, move.y) * Time.deltaTime * movementSpeedMultiplier;
+        transform.Translate(m, Space.World);
+    }
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+    }
+    private void OnDisable()
+    {
+        controls.Gameplay.Disable();
+    }
+
+}
