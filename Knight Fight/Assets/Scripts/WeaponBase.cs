685308886cd579aa51a65ff45302defbc5546a49
jockek2 <joakim.karlsteen@gmail.com>
2020-04-02 09:59:11 +0200

    Sväedscript och basscript för vapen

---
+++ Knight Fight/Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,40 @@
+﻿// Author Joakim Karlsteen 
+// 01-04-2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+  
+
+    public int damage;
+    public float attackspeed;
+    public int spawnRarity;
+    public float attackrange = 0.5f;
+    
+
+    // Meleeattack kollar om en sfär överlappar med en annan spelare om så är fallet så tar spelaren dmg
+    public virtual void MeleeAttack(Transform attackPoint, float attackRange)
+    {
+
+        Collider[] hitPlayer = Physics.OverlapSphere(attackPoint.position, attackrange);
+        foreach (Collider enemy in hitPlayer)
+        {
+            //Scriptet med removehelth i RemoveHealth(damage);
+            RemoveHealth(damage);
+        }
+    }
+    public virtual void RangedAttack()
+    {
+        //kommer overrideas i själva vapen scriptet
+    }
+    public void RemoveHealth(int damage)
+    {
+        Debug.Log("hit");
+
+    }
+
+
+}
+
---
+++ Knight Fight/Assets/Scripts/WeaponSword.cs
@@ -0,0 +1,34 @@
+﻿// Author Joakim Karlsteen 
+// 01-04-2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSword : WeaponBase
+{
+
+   public Transform attackPoint;
+   //public Animation Sword;
+    
+   void Update()
+    {
+        //SwingSword();
+    }
+
+
+   void SwingSword()
+    {   
+        
+        MeleeAttack(attackPoint, attackrange);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackrange);
+        
+    }
+
+   
+
+
+}
