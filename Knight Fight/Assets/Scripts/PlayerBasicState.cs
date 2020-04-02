c00df425bbe6de72555cf160583c953a3fa6a5ee
Patrik P <prutte94@gmail.com>
2020-04-02 23:30:46 +0200

    Revert "asd"
    This reverts commit 145365631a491f6542d37ddab9301329947a8bb4.

---
+++ Knight Fight/Assets/Scripts/PlayerBasicState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBasicState : PlayerIState
+{
+    private readonly PlayerStatePattern player;
+
+    public PlayerBasicState(PlayerStatePattern statePatternPlayer)
+    {
+        player = statePatternPlayer;
+    }
+    public void UpdateState()
+    {
+        player.Movement();
+
+    }
+    public void ChangeState(PlayerIState newState)
+    {
+        if (player.ValidStateChange(newState))
+        {
+            player.currentState = newState;
+        }
+    }
+}
---
+++ Knight Fight/Assets/script/goDownLvl2.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goDownLvl2 : MonoBehaviour
+{
+    public GameObject ParticleEffectprefab;
+    private float randomTimer;
+    private float goDownTimer;
+    private Vector3 startPos;
+    private bool hasStartedParticleSystem;
+    private bool hasDropped;
+    [SerializeField]
+    private bool isMiddle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        if(isMiddle)
+        {
+            goDownTimer = 40;
+        }
+        else if(!isMiddle)
+        {
+            goDownTimer = 20;
+        }
+        
+        hasDropped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(goDownTimer);
+        goDownTimer -= Time.deltaTime;
+        if (goDownTimer < 5 && !hasStartedParticleSystem)
+        {
+            GameObject particleEffect = Instantiate(ParticleEffectprefab, transform.position + new Vector3(0, 70), Quaternion.identity) as GameObject;
+            hasStartedParticleSystem = true;
+            Destroy(particleEffect, 5);
+
+        }
+        if (goDownTimer <= 0 && goDownTimer > -5)
+        {
+            transform.Translate(Vector3.down * 100 * Time.deltaTime);
+            hasDropped = true;
+        }
+        if (goDownTimer <= -5)
+        {
+            if (transform.position.y != startPos.y)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPos, 100 * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(Vector3.zero);
+            }
+
+
+        }
+        if (transform.position.y == startPos.y && hasDropped)
+        {
+            if (isMiddle)
+            {
+                goDownTimer = 40;
+            }
+            else
+            {
+                goDownTimer = 30;
+            }
+            hasDropped = false;
+        }
+    }
+}
