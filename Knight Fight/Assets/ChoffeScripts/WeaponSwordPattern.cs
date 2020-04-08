b5170cda02b2deff556634650a8af0a90c0f9baf
jockek2 <joakim.karlsteen@gmail.com>
2020-04-08 13:41:59 +0200

    Attack i sword bör fungera

--- Knight Fight/Assets/ChoffeScripts/WeaponSwordPattern.cs
+++ Knight Fight/Assets/ChoffeScripts/WeaponSwordPattern.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WeaponSwordPattern : WeaponBaseClass
 {
+    
+    public float attackZone;
+    public float durabilityDecrement;
     // FIXA BUGGAR FÖRST INNAN DU FORTSÄTTER, SPELAREN ROTERAR INNAN VAPNET KASTAS IVÄG OCH DET ÄR FUCKING WEIRD. VAPNET KASTAS INTE ALLTID RAKT FRAM HELLER
     private float currentDurability;
 
@@ -29,7 +32,25 @@ public class WeaponSwordPattern : WeaponBaseClass
 
     public override void Attack()
     {
-        //do this weapons specific attack
+        Collider[] hitPlayer = Physics.OverlapSphere(this.transform.position, attackZone); 
+        foreach (Collider enemy in hitPlayer)
+        {
+            // delar endast ut dmg på spelaren och inte andra objekt som råkar bli träffad och inte på sig själv
+            if (enemy.gameObject.layer == 8)
+            {
+                if (enemy.gameObject.name == this.gameObject.GetComponentInParent<GameObject>().name) 
+                {
+                    //Gör inget 
+                    // Debug.Log(attackingPlayer.name + " Hit my self " + enemy.gameObject.name); 
+                }
+                else
+                {
+                   ChangeDurability(durabilityDecrement);
+                   enemy.gameObject.GetComponent<PlayerStatePattern>().OnHit(damage);
+                }
+            }
+        }
+        Debug.Log("Attack");
     }
 
     public override void ThrownAttack(Collision col)
@@ -45,5 +66,13 @@ public class WeaponSwordPattern : WeaponBaseClass
     public override void OnCollisionEnter(Collision collision)
     {
         currentState.HandleCollision(collision);
+    }
+
+    //visuellt visa träffzonen
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, attackZone);
+
     }
 }
