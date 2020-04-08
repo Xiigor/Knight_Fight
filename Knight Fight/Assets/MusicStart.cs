eb7081ea83796816d88bc93971d24ce855d00a10
Patrik P <prutte94@gmail.com>
2020-04-08 11:18:05 +0200

    MusicScript

---
+++ Knight Fight/Assets/MusicStart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class MusicStart : MonoBehaviour
+{
+    public StudioEventEmitter gameplayMusic;
+
+    void Awake()
+    {
+        gameplayMusic.Play();
+    }
+
+}
+
