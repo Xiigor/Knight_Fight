92049ed0b037fe274aeda1ceaf4f4c8efbaf3edc
JoacimO <Thix61@gmail.com>
2020-04-14 16:43:45 +0200

    Fish level

---
+++ Knight Fight/Assets/Orlovscript/Fish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float jumpTimer;
+    private Vector3 RandomDir;
+    void Start()
+    {
+        jumpTimer = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpTimer -= Time.deltaTime;
+        if(jumpTimer <= 0)
+        {
+            GetComponent<Rigidbody>().AddForce(Random.Range(-249f, 249f) * 50, 2000 * Time.deltaTime, Random.Range(-249f, 249f) * 50);
+            transform.Rotate(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+            jumpTimer = 0.5f;
+        }
+    }
+}
