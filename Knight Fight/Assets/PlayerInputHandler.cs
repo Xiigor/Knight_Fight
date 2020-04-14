135e73df56126352cd66ab8d44c592d2fcc5cc68
Martin Quach <martin_quach@hotmail.com>
2020-04-14 11:15:38 +0200

    Co-Op/Instansiering av flera spelare
    Borde kunna instansiera flera spelare nu, i alla fall två för tillfället

---
+++ Knight Fight/Assets/PlayerInputHandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerMover mover;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        var movers = FindObjectsOfType<PlayerMover>();  //Assign the right mover to the current playerInputHandler that gets "spawned".
+        var index = playerInput.playerIndex;    //index 0 = player 1, index 1 = player 2 osv
+
+
+        mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
+        //Gets our PlayerMover object
+        //mover = GetComponent<PlayerMover>();
+    }
+
+    public void OnMove(CallbackContext context)
+    {
+        mover.SetInputVector(context.ReadValue<Vector2>());
+    }
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        //Checks input in x and y axis, sets the InputVector to the mover
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        mover.SetInputVector(new Vector2(x, y));
+    }
+    */
+}
---
+++ Knight Fight/Assets/PlayerMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 3f;
+
+    [SerializeField]
+    private int playerIndex = 0;
+
+    private CharacterController controller;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector2 inputVector = Vector2.zero;
+
+    private void Awake ()
+    {
+        //Sets up the controller on Awake
+        controller = GetComponent<CharacterController>();
+    
+    }
+
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+    public void SetInputVector(Vector2 direction)
+    {
+        //Sets the direction where we want to move
+        inputVector = direction;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Target directions * moveSpeed
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= moveSpeed;
+
+        //Controller moves according to the target direction
+        controller.Move(moveDirection * Time.deltaTime);
+        
+    }
+}
