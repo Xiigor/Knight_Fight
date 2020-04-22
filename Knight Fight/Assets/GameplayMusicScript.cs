10cce10bcc2663a49cb524dd10d1133b010c9e3f
Patrik P <prutte94@gmail.com>
2020-04-22 12:11:37 +0200

    Unity + FMOD Update

---
+++ Knight Fight/Assets/GameplayMusicScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class GameplayMusicScript : MonoBehaviour
+{
+    public StudioEventEmitter gameplayMusic;
+    public string paraName;
+    
+    void Awake()
+    {
+        gameplayMusic.Play();
+    }
+}
---
+++ Knight Fight/Assets/MenuMusicScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class MenuMusicScript : MonoBehaviour
+{
+    public StudioEventEmitter menuMusic;
+
+    private void Awake()
+    {
+        menuMusic.Play();
+    }
+
+
+}
