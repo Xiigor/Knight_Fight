347c76a7b1b9865435a1efd03e2f1dd708d04053
Patrik P <prutte94@gmail.com>
2020-04-22 13:02:32 +0200

    asd

---
+++ Knight Fight/Assets/ChoffeScripts/WeaponUnequippedState.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponUnequippedState : WeaponIState
+{
+    private readonly WeaponBaseClass weapon;
+
+    public WeaponUnequippedState(WeaponBaseClass weaponBase)
+    {
+        weapon = weaponBase;
+    }
+
+    public void OnStateEnter()
+    {
+        weapon.gameObject.layer = weapon.UnequippedLayer;
+        ChangePhysics();
+        if (weapon.parentPlayer != null)
+        {
+            Physics.IgnoreCollision(weapon.parentPlayer.GetComponent<Collider>(), weapon.col, false);
+            weapon.parentPlayer = null;
+        }
+        weapon.gameObject.tag = weapon.weaponTag;
+
+    }
+    public void UpdateState()
+    {
+
+    }
+    public void ChangePhysics()
+    {
+        weapon.rb.isKinematic = false;
+        weapon.col.enabled = true;
+        weapon.rb.useGravity = true;
+    }
+
+    public void HandleCollision(Collision col)
+    {
+       if(col.gameObject.tag == weapon.playerTag)
+        {
+            //if the player is not holding a weapon already, pick up this one
+            if(col.gameObject.GetComponent<PlayerStatePattern>().weapon == null)
+            {
+                weapon.SetParentPlayer(col);
+
+                col.gameObject.GetComponent<PlayerStatePattern>().PickupItem(weapon.gameObject);
+                weapon.ChangeState(weapon.equippedState);
+            }
+        }
+    }
+   
+}
