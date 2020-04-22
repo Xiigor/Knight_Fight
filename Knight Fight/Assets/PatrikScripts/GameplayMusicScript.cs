6c4823fa1c649cdfefa646a7b1d526545ff93315
Patrik P <prutte94@gmail.com>
2020-04-22 18:02:00 +0200

    Asd

---
+++ Knight Fight/Assets/PatrikScripts/GameplayMusicScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class GameplayMusicScript : MonoBehaviour
+{
+    public StudioEventEmitter gameplayMusic;
+    public string ParaName;
+ 
+    private void Awake()
+    {
+        gameplayMusic.Play();
+    }
+
+    void Start()
+    {
+        GameObject.FindGameObjectsWithTag("Player");
+        Debug.Log("Found Player");
+    }
+    
+
+    
+
+}
---
+++ Knight Fight/Assets/PatrikScripts/MenuMusicScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+public class MenuMusicScript : MonoBehaviour
+{
+    public StudioEventEmitter menuMusic;
+
+    private void Awake()
+    {
+        menuMusic.Play();
+    }
+}
