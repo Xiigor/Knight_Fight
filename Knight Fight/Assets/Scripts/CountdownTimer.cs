d2056e961c2088a930f80df6d1dd1b2931402522
Martin Quach <martin_quach@hotmail.com>
2020-04-29 12:26:54 +0200

    Ändrade filer

---
+++ Knight Fight/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 5f;
+
+    public bool countdownIsDone = false;
+    //public countdownTimer countdownBool;
+
+    [SerializeField]
+    Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            countdownIsDone = true;
+        }
+        //Sätt detta i movement
+        //if (countdownBool.countdownIsDone == true)
+        //{
+        //    Apply movement
+        //}
+    }
+}
