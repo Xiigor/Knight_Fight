108ebe598be1db2e9a8d4be6af3852161f8648fa
Martin Quach <martin_quach@hotmail.com>
2020-05-11 11:49:22 +0200

    Broken Merge

---
+++ Knight Fight/Assets/ChoffeScripts/PlayerDeadState.cs
@@ -0,0 +1,60 @@
+﻿using FMODUnity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeadState : PlayerIState
+{
+
+    private readonly PlayerStatePattern player;
+    public float internalStateTimer;
+
+    public PlayerDeadState(PlayerStatePattern statePatternPlayer)
+    {
+        player = statePatternPlayer;
+    }
+
+    public void OnStateEnter()
+    {
+        float closestDistance = Mathf.Infinity;
+        GameObject closestCrowd = null;
+        for (int i = 0; i < player.crowdParent.childCount; i++)
+        {
+            Transform crowd = player.crowdParent.GetChild(i);
+            float distance = Vector3.Distance(player.transform.position, crowd.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestCrowd = crowd.gameObject;
+            }
+
+        }
+
+        closestCrowd.GetComponent<AudioCrowd>().Cheer();
+
+        //Förmodligen loppa crowd här så att dem jublar
+
+        player.gameManager.alivePlayers.Remove(player.gameObject);
+        player.EnableRagdoll();
+        player.tag = player.deadPlayerTag;
+    }
+
+    public void UpdateState()
+    {
+
+    }
+    public void ChangeState(PlayerIState newState)
+    {
+        if (newState == player.basicState)
+        {
+            player.currentState = newState;
+        }
+        else
+            Debug.Log("GCD Trigger");
+    }
+    public void TakeDamage(float damage)
+    {
+        //cant be dealt more damage
+    }
+}
