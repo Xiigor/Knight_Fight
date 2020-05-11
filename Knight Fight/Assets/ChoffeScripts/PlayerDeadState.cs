b3bf112f18fa37e2af3af8125c62a1bc08036bcd
Martin Quach <martin_quach@hotmail.com>
2020-05-11 10:40:39 +0200

    Publik Jubel WIP

--- Knight Fight/Assets/ChoffeScripts/PlayerDeadState.cs
+++ Knight Fight/Assets/ChoffeScripts/PlayerDeadState.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections;
+﻿using FMODUnity;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +17,23 @@ public class PlayerDeadState : PlayerIState
 
     public void OnStateEnter()
     {
+        float closestDistance = Mathf.Infinity;
+        GameObject closestCrowd = null;
+        for (int i = 0; i < player.crowdParent.childCount; i++)
+        {
+            Transform crowd = player.crowdParent.GetChild(i);
+            float distance = Vector3.Distance(player.transform.position, crowd.position);
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestCrowd = crowd.gameObject;
+            }
+
+        }
+
+        closestCrowd.GetComponent<AudioCrowd>().Cheer();
+        
+        //Förmodligen loppa crowd här så att dem jublar
 
     }
 
---
+++ Knight Fight/Assets/Scripts/AudioCrowd.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioCrowd : MonoBehaviour
+{
+    [EventRef]
+    public string crowdCheer;
+    public FMOD.Studio.EventInstance cheer;
+
+    public void Cheer()
+    {
+        RuntimeManager.PlayOneShot(crowdCheer, transform.position);
+    }
+}
