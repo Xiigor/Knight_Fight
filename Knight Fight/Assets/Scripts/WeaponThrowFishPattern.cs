cba01c2513051ad0130791220efd80a730a8b8ac
JoacimO <Thix61@gmail.com>
2020-05-12 11:14:32 +0200

    tweaking

--- Knight Fight/Assets/Scripts/WeaponThrowFishPattern.cs
+++ Knight Fight/Assets/Scripts/WeaponThrowFishPattern.cs
@@ -6,7 +6,13 @@ public class WeaponThrowFishPattern : WeaponBaseClass
 {
     public float durabilityDecrement;
     public GameObject weaponAmmo;
+    public int numberofAmmoToSpawn;
+    public float spawnOfsettDist;
+    public enum LaunchDir { forward, up, left, right };
+    public LaunchDir launchDir;
     private float currentDurability;
+    private float spawnOffset = 0;
+    private int increaseSpawnOffsett = 1;
   
     private void Awake()
     {
@@ -14,6 +20,7 @@ public class WeaponThrowFishPattern : WeaponBaseClass
         equippedState = new WeaponEquippedState(this);
         thrownState = new WeaponThrownState(this);
         thisWepType = Weapontype.spellbook;
+        
     }
 
     private void Start()
@@ -29,31 +36,44 @@ public class WeaponThrowFishPattern : WeaponBaseClass
     private void Update()
     {
         currentState.UpdateState();
-        if(internalAttackTimer >= animationDuration)
-        {
-            Attack();
-        }
     }
 
     public override void Attack()
     {
-        internalAttackTimer = 0f;
-        if (attackActive)
+        //launch projectile and instanciate projectile 
+        // Projectile i eget script med en OnCollisonEnter kollar om träffat en spelare och isfall gå in i enemy.gameObject.GetComponent<PlayerStatePattern>().OnHit(damage);
+        ChangeDurability(durabilityDecrement);
+        //Instantiate(weaponAmmo);
+        for (int i = 0; i < numberofAmmoToSpawn; i++)
         {
-            //launch projectile and instanciate projectile 
-            // Projectile i eget script med en OnCollisonEnter kollar om träffat en spelare och isfall gå in i enemy.gameObject.GetComponent<PlayerStatePattern>().OnHit(damage);
-            ChangeDurability(durabilityDecrement);
-            //Instantiate(weaponAmmo);
             audioPlayer.Attacking();
+            Vector3 spawnOffsetVec = new Vector3(0,0,0);
             GameObject temp = Instantiate(weaponAmmo, parentPlayer.GetComponent<PlayerStatePattern>().projectileSpawnPos.transform.position, Quaternion.identity);
+            //spawnOffsetVec = temp.transform.localPosition - parentPlayer.transform.position;
+            //Debug.Log(spawnOffsetVec);
+            //spawnOffsetVec.x = spawnOffsetVec.x - spawnOffset * spawnOffsetVec.z / Mathf.Sqrt(spawnOffsetVec.x * spawnOffsetVec.x + spawnOffsetVec.z * spawnOffsetVec.z);
+            //spawnOffsetVec.z = spawnOffsetVec.z - spawnOffset * spawnOffsetVec.x / Mathf.Sqrt(spawnOffsetVec.x * spawnOffsetVec.x + spawnOffsetVec.z * spawnOffsetVec.z);
+            //temp.transform.localPosition = spawnOffsetVec + parentPlayer.transform.position;
+            //spawnOffsetVec.z = spawnOffset * temp.transform.localPosition.x / (Mathf.Sqrt(temp.transform.localPosition.x* temp.transform.localPosition.x + temp.transform.localPosition.z * temp.transform.localPosition.z));
+            //spawnOffsetVec.x = spawnOffset * temp.transform.localPosition.z / (Mathf.Sqrt(temp.transform.localPosition.x * temp.transform.localPosition.x + temp.transform.localPosition.z * temp.transform.localPosition.z));
+            //temp.transform.localPosition = temp.transform.localPosition + spawnOffsetVec;
+            
             temp.GetComponent<ProjectileFish>().parentObject = parentPlayer.GetComponent<PlayerStatePattern>().projectileSpawnPos;
+            temp.GetComponent<ProjectileFish>().spellBook = this.gameObject;
+            spawnOffset = spawnOffset * (-1);
+            Debug.Log(parentPlayer.GetComponent<PlayerStatePattern>().projectileSpawnPos.transform.forward);
+            increaseSpawnOffsett++;
+            if (increaseSpawnOffsett == 2)
+            {
+               
+                spawnOffset += spawnOfsettDist;
+                increaseSpawnOffsett = 0;
+            }
+            
             //transform.DetachChildren();
-            attackActive = false;
         }
-        else
-            attackActive = true;
+        spawnOffset = 0;
     }
-
 
     public override void ChangeDurability(float durabilityDecrement)
     {
