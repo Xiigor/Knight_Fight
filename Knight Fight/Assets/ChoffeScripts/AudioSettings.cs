fa58861cd541c9b5183480e693fb53a60114609c
Xiigor <47105441+Xiigor@users.noreply.github.com>
2020-05-13 16:21:45 +0200

    working audiomenu

--- Knight Fight/Assets/ChoffeScripts/AudioSettings.cs
+++ Knight Fight/Assets/ChoffeScripts/AudioSettings.cs
@@ -7,57 +7,86 @@ using TMPro;
 
 public class AudioSettings : MonoBehaviour
 {
-    public Slider masterVolume;
+    public Slider masterVolumeSlider;
+    public float masterVolume = 1f;
     public TextMeshProUGUI masterVolumeText;
 
-    public Slider sfxVolume;
+    public Slider sfxVolumeSlider;
+    public FMOD.Studio.VCA sfx;
+    public float sfxVolume = 50f;
     public TextMeshProUGUI sfxVolumeText;
 
-    public Slider musicVolume;
+    public Slider musicVolumeSlider;
+    public FMOD.Studio.VCA music;
+    public float musicVolume = 50f;
     public TextMeshProUGUI musicVolumeText;
 
-    public Slider commentatorVolume;
+    public Slider commentatorVolumeSlider;
+    public FMOD.Studio.VCA commentator;
+    public float commentatorVolume = 50f;
     public TextMeshProUGUI commentatorVolumeText;
 
-    public Slider crowdVolume;
+    public Slider crowdVolumeSlider;
+    public FMOD.Studio.VCA crowd;
+    public float crowdVolume = 50f;
     public TextMeshProUGUI crowdVolumeText;
 
     [Range(0.001f,1f)] public float test = 0.5f;
 
-    private void Start()
+    private void Awake()
     {
+        sfx = RuntimeManager.GetVCA("VCA:/Sfx");
+        music = RuntimeManager.GetVCA("VCA:/Music");
+        commentator = RuntimeManager.GetVCA("VCA:/Commentators");
+        crowd = RuntimeManager.GetVCA("VCA:/Crowd");
 
+        ChangeSFX(sfxVolume);
+        ChangeMusic(musicVolume);
+        ChangeCommentator(commentatorVolume);
+        ChangeCrowd(crowdVolume);
+    }
+    public void Update()
+    {
+        sfx.setVolume(sfxVolume * masterVolume);
+        music.setVolume(musicVolume * masterVolume);
+        commentator.setVolume(commentatorVolume * masterVolume);
+        crowd.setVolume(crowdVolume * masterVolume);
     }
 
     public void ChangeMaster(float change)
     {
-        masterVolume.value = change;
-        //ändra fmod inställningen
-        SetPercentage(masterVolume.value, masterVolumeText);
+        masterVolumeSlider.value = change;
+        masterVolume = change / 100;
+        SetPercentage(masterVolumeSlider.value, masterVolumeText);
     }
 
     public void ChangeSFX(float change)
     {
-        sfxVolume.value = change;
-        SetPercentage(sfxVolume.value, sfxVolumeText);
+        sfxVolumeSlider.value = change;
+        sfxVolume = change / 100;
+        SetPercentage(sfxVolumeSlider.value, sfxVolumeText);
     }
 
     public void ChangeMusic(float change)
     {
-        musicVolume.value = change;
-        SetPercentage(musicVolume.value, musicVolumeText);
+        musicVolumeSlider.value = change;
+        musicVolume = change / 100;
+        SetPercentage(musicVolumeSlider.value, musicVolumeText);
     }
 
     public void ChangeCommentator(float change)
     {
-        commentatorVolume.value = change;
-        SetPercentage(commentatorVolume.value, commentatorVolumeText);
+        commentatorVolumeSlider.value = change;
+        commentatorVolume = change / 100;
+        SetPercentage(commentatorVolumeSlider.value, commentatorVolumeText);
     }
 
     public void ChangeCrowd(float change)
     {
-        crowdVolume.value = change;
-        SetPercentage(crowdVolume.value, crowdVolumeText);
+        crowdVolumeSlider.value = change;
+
+        crowdVolume = change / 100;
+        SetPercentage(crowdVolumeSlider.value, crowdVolumeText);
     }
 
     public void SetPercentage(float value, TextMeshProUGUI text)
