a66dd79d0a73bf6778bb43246848a5eef63161e1
JoacimO <Thix61@gmail.com>
2020-05-13 15:34:24 +0200

    tweaking + new weapons

--- Knight Fight/Assets/Scripts/ProjectileFish.cs
+++ Knight Fight/Assets/Scripts/ProjectileFish.cs
@@ -4,23 +4,20 @@ using UnityEngine;
 
 public class ProjectileFish : ProjectileBase
 {
-    GameObject playerPos;
+    
     // Start is called before the first frame update
     private void Awake()
     {
         flyingState = new ProjectileFlyingState(this);
         groundedState = new ProjectileGroundedState(this);
         rb = GetComponent<Rigidbody>();
-        //parentObject = transform.parent.gameObject;
-        //Player = parentObject.GetComponent<WeaponThrowFishPattern>().parentPlayer.GetComponent<PlayerStatePattern>().rightHandGameobject;
-        //playerPos = parentObject.GetComponent<WeaponThrowFishPattern>().parentPlayer;
-        projectileTransform = gameObject.transform;  
+        //spellBook = pa 
+       
     }
 
     private void Start()
     {
         currentState = flyingState;
-        //Physics.IgnoreLayerCollision(Player.layer, gameObject.layer,true);
         LaunchPos(Player);
     }
 
@@ -32,12 +29,9 @@ public class ProjectileFish : ProjectileBase
 
     public override void LaunchPos(GameObject parent)
     {
-        //sätter projektilen på spelarens hand Kommer hit efter initsieringen av projektilen 
-        //GameObject SpawnPos = playerPos.transform.Find("ProjectileSpawnPoint").gameObject;
-        //projectileTransform.position = SpawnPos.transform.position;
-        //projectileTransform.rotation = playerPos.transform.rotation;
+        
         StateChanger(flyingState);
-        //LaunchFish();
+        
     }
 
     public override void StateChanger(ProjectileIState newState)
