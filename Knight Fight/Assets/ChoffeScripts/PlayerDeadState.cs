63b09e7a84be5f1fd34fa979ab7f923a6f443f03
Xiigor <47105441+Xiigor@users.noreply.github.com>
2020-05-15 14:50:31 +0200

    publik cheer

--- Knight Fight/Assets/ChoffeScripts/PlayerDeadState.cs
+++ Knight Fight/Assets/ChoffeScripts/PlayerDeadState.cs
@@ -18,6 +18,19 @@ public class PlayerDeadState : PlayerIState
         player.gameManager.alivePlayers.Remove(player.gameObject);
         player.EnableRagdoll();
         player.tag = player.deadPlayerTag;
+        float closestDistance = Mathf.Infinity;
+        GameObject closestCrowd = null;
+        for(int i = 0; i < player.crowdParent.childCount; i++)
+        {
+            Transform crowd = player.crowdParent.GetChild(i);
+            float distance = Vector3.Distance(player.transform.position, crowd.position);
+            if(distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestCrowd = crowd.gameObject;
+            }
+        }
+        closestCrowd.GetComponent<AudioCrowd>().Cheer();
     }
 
     public void UpdateState()
